d652ab997059599aa1ce1370e4c4979d2ec1d938
Dev Varshney <dev05varshney@gmail.com>
2024-10-26 23:28:37 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -86,7 +86,7 @@ fi
 
 echo ""
 echo -e "${YELLOW}Installing AAPAnel On This Server${NC}"
-wget -O install.sh https://raw.githubusercontent.com/WebManagerSSH/aaPanel/main/script/install-ubuntu_6.0_en.sh && bash install.sh github
+wget -O install.sh https://raw.githubusercontent.com/weby-dev/aapanel-installer/refs/heads/main/install-ubuntu_6.0_en.sh && bash install.sh github
 
 echo ""
 echo -e "${GREEN}All Done...Goodbye!${NC}"
